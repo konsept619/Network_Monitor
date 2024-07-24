dc43d788d3439c8e219361b6495ca4e0b976c410
konsept619 <konrad.wrzesien58@gmail.com>
2024-07-24 14:46:20 +0200

    Veryfing if host is up and implementing result to txt file

--- packet_analisis.sh
+++ packet_analisis.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+ping_and_record(){
+  pinging_output=$(ping -c 3 -q 192.1.1.1 &>/dev/null)
+  if [ $? -eq 0 ]; then 
+  echo "$pinging_output" | tail -1 | cut -d ' ' -f 4 | tr \['/'] \[','] >> network_data.txt
+  return 0
+else
+  #here we can add some output indicating unavailability of host in text file
+  echo "0,0,0,0" >> network_data.txt
+  return 1
+  fi
+}
+
+isUP=0 #flag for indicating if post is up right now 
+
 while [ 1 -gt 0 ];
 do
-
-  ping -c 5 -q 1.1.1.1 | tail -1 | cut -d ' ' -f 4 | tr \['/'] \[','] >> network_data.txt
+  ping_and_record
+  if [ $? -eq 0 ]; 
+  then
+    echo "Host is up"
+    let isUP=1
+  else
+    echo "Host is down/unreachable"
+    let isUP=0
+  fi
 
 done
