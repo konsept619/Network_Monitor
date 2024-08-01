4c706835b0e66fe8ba85e870ed1cbeb3388c1e7d
konsept619 <konrad.wrzesien58@gmail.com>
2024-08-01 11:55:32 +0200

    Now every record has timestamp

--- packet_analisis.sh
+++ packet_analisis.sh
@@ -8,29 +8,31 @@ if [ $CHECKIP != 0 ]; then
 fi
 
 ping_and_record(){
+  DATE=$( date +"%D %T" )
   pinging_output=$(ping -c 3 $IPADDRESS 2>/dev/null)
   if [ $? -eq 0 ]; then 
-  echo "$pinging_output" | tail -1 | cut -d ' ' -f 4 | tr \['/'] \[','] >> network_data.txt
+    OUTPUT=$( echo "$pinging_output" | tail -1 | cut -d ' ' -f 4 | tr \['/'] \[','] ) 
+    echo "$OUTPUT,$DATE" >> $IPADDRESS-network_data.txt #every file has unique name containing IP of a target to make it more clear
   return 0
 else
   #here we can add some output indicating unavailability of host in text file
-  echo "0,0,0,0" >> network_data.txt
+  echo "0,0,0,0" >> $IPADDRESS-network_data.txt
   return 1
   fi
 }
 
 isUP=0 #flag for indicating if post is up right now 
 
-while [ 1 -gt 0 ];
+while true;
 do
   ping_and_record
   if [ $? -eq 0 ]; 
   then
     echo "Host is up"
-    let isUP=1
+    (( isUP=1 ))
   else
     echo "Host is down/unreachable"
-    let isUP=0
+    (( isUP=0 ))
   fi
 
 done
