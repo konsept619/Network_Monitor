732ab8424d206726b9543264a0e00a203ca049d1
konsept619 <konrad.wrzesien58@gmail.com>
2024-08-01 14:37:40 +0200

    Creating similar script which checks bandwidth with iperf3

---
+++ bandwidth_analisis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+IPADDRESS=$1
+
+CHECKIP=$( ip route get $IPADDRESS &>/dev/null ; echo $? )
+if [ $CHECKIP != 0 ]; then
+  exit
+fi
+
+check_bandwidth(){
+  DATE=$( date +"%D %T" )
+  checking_bandwidth_iperf=$( iperf3 -c $IPADDRESS -Z 2>/dev/null )
+  if [ $? -eq 0 ]; then 
+    OUTPUT=$( echo "$checking_bandwidth_iperf" | tail -3 | head -1 | awk '{print $7}' )
+    echo "$OUTPUT, $DATE" >> $IPADDRESS-bandwidth_data.txt
+    return 0 
+  else
+    echo "000,$DATE" >> $IPADDRESS-bandwidth_data.txt
+    return 1
+  fi
+}
+while true;
+do
+check_bandwidth
+done
