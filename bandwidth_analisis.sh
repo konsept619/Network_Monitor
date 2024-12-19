d46664a431de68881c8fc91ab8f04d8eae5f996d
konsept619 <konrad.wrzesien58@gmail.com>
2024-12-19 09:20:05 +0100

    Adding new location for logs

--- bandwidth_analisis.sh
+++ bandwidth_analisis.sh
@@ -3,7 +3,7 @@
 if [ -e ./netparams.conf ]; then
   . ./netparams.conf
 else 
-  echo "Configuration file doesn't exist or its name was changed! Make sure proper file is placed in working directory and has a proper name."
+  echo "Configuration file doesn't exist or its name was changed! Make sure a proper file is placed in working directory and has a proper name."
   exit 1
 fi
 
@@ -18,12 +18,12 @@ check_bandwidth(){
   checking_bandwidth_iperf=$( iperf3 -c $IPADDRESS -Z -u 2>/dev/null )
   if [ $? -eq 0 ]; then 
     OUTPUT=$( echo "$checking_bandwidth_iperf" | tail -3 | head -1 | awk '{print $7}' )
-    echo "$OUTPUT, $DATE" >> $IPADDRESS-bandwidth_data.txt
-    mosquitto_pub -h $IP_BROKER -t stats/ping -m "$OUTPUT,$DATE"
+    echo "$OUTPUT, $DATE" >> "./logs/$IPADDRESS-bandwidth_data.txt"
+    mosquitto_pub -h $IP_BROKER -t $TOPIC_IPERF -m "$OUTPUT,$DATE"
     return 0 
   else
-    echo "000,$DATE" >> $IPADDRESS-bandwidth_data.txt
-    mosquitto_pub -h $IP_BROKER -t stats/ping -m "000,$DATE"
+    echo "000,$DATE" >>  "./logs/$IPADDRESS-bandwidth_data.txt"
+    mosquitto_pub -h $IP_BROKER -t $TOPIC_IPERF -m "000,$DATE"
     return 1
   fi
 }
--- packet_analisis.sh
+++ packet_analisis.sh
@@ -19,14 +19,14 @@ ping_and_record(){
   if [ $? -eq 0 ]; then 
     OUTPUT=$( echo "$pinging_output" | tail -1 | cut -d ' ' -f 4 | tr \['/'] \[','] ) 
     LOST_PACKETS=$( echo "$pinging_output" | tail -2 | head -1 | awk '{print $1-$4}' ) 
-    echo "$OUTPUT,$LOST_PACKETS,$DATE" >> $IPADDRESS-network_data.txt #every file has unique name containing IP of a target to make it more clear
-    mosquitto_pub -h $IP_BROKER -t stats/ping -m "$OUTPUT,$LOST_PACKETS,$DATE"
+    echo "$OUTPUT,$LOST_PACKETS,$DATE" >> "./logs/$IPADDRESS-network_data.txt" #every file has unique name containing IP of a target to make it more clear
+    mosquitto_pub -h $IP_BROKER -t $TOPIC_PING -m "$OUTPUT,$LOST_PACKETS,$DATE"
     return 0
-else
-  #here we can add some output indicating unavailability of host in text file
-  echo "0,0,0,0,$DATE" >> $IPADDRESS-network_data.txt
-  mosquitto_pub -h $IP_BROKER -t stats/ping -m "0,0,0,0,$DATE"
-  return 1
+  else
+    #here we can add some output indicating unavailability of host in text file
+    echo "0,0,0,0,$DATE" >>  "./logs/$IPADDRESS-network_data.txt"     
+    mosquitto_pub -h $IP_BROKER -t $TOPIC_PING -m "0,0,0,0,$DATE"
+    return 1
   fi
 }
 
